ee13b8afeb1f869536918d7c7b861374132c3e96
rwhudson119 <rwhudson@student.unimelb.edu.au>
2022-01-22 01:24:33 +1100

    Fixed loading issue
    had an issue where page would load before data was collected and would cause an error, is fixed :))

--- src/components/addAlbum.js
+++ src/components/addAlbum.js
@@ -1,42 +1,57 @@
-import { useState, useEffect } from "react";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "../services/backendApi.js";
-import { Link, useParams  } from 'react-router-dom';
-const headers = {
-    "Access-Control-Allow-Origins": "*"
-}
+import { useParams } from 'react-router-dom';
 
 
 const AddAlbum = (props) => {
 
     let params = useParams()
 
-    const [results, setResults] = useState([]);
-    const URL = `https://api.deezer.com/album/${params.albumId}`
-
-
-    const GetAlbum = () => {
-        axios.get(URL).then(res => {
-            setResults(res.data)
-        }
-    )}
-
+    const [results, setResults] = useState(null);
+    
     useEffect(() => {
+        const URL = `https://api.deezer.com/album/${params.albumId}`
+
+        const GetAlbum = async () => {
+
+            const res = await axios({
+                method: 'get',
+                url: URL,
+                headers: {
+                  'Access-Control-Allow-Origin': '*',
+                },
+            });
+            setResults(res.data)
+            console.log(res.data)
+            console.log("hi1")
+        };
         GetAlbum();
-        
     }, []);
 
-    //need a async thing so that once everything loads then it displays
+
+    if (results == null){
+        console.log("Returning null");
+        return null;
+    }
+    if (results instanceof Error) {
+        console.error(results);
+        return (
+                    <div>
+                        <h1>Error.</h1>
+                    </div>
+                );
+    }
 
     return (
     <div className="App">
         <header className="App-header">
             <img src= {results.cover}/>
             <p> {results.title}</p>
+            <p> {results.release_date}</p>
+            <p> {results.genres.data[0].name}</p>
         </header>
     </div>
     )
 }
-
 
 export default AddAlbum
